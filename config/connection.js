eb05e9d436168b935e454d008ec962fdae66ebe9
Seymore Biggins <zachharris024@gmail.com>
2020-11-02 11:53:16 -0600

    connect db and setup npm

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// require sequelize constructor
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// connect to db and pass in mysql info
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
+
+module.exports = sequelize;
